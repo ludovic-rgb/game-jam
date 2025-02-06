ffa56ac4b94fb154918611756db85017eb735824
KMELZB <melvynb2904@outlook.fr>
2025-02-06 16:40:38 +0100

    Add files via upload

--- game-jam/scripts/reglage_volume.gd
+++ game-jam/scripts/reglage_volume.gd
@@ -4,18 +4,17 @@ extends HSlider
 @export
 var bus_name : String
 
-var bus_index : int
+var bus_index : int 
 
 func _ready() :
 	bus_index = AudioServer.get_bus_index(bus_name)
-	value_changed.connect(_on_value_changed);
+	value_changed.connect(_on_value_changed)
 	
 	value = db_to_linear(
 		AudioServer.get_bus_volume_db(bus_index)
 	)
 
-
-func _on_value_changed(value: float) -> void:
+func _on_value_changed(value: float) :
 	AudioServer.set_bus_volume_db(
 		bus_index,
 		linear_to_db(value)

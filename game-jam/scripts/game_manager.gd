eea35b30e68325634ec6615067e599635775e60b
ludovic-rgb <ludovic.gasperini@my-digital-school.org>
2025-02-06 15:15:27 +0100

    update

--- game-jam/scripts/game_manager.gd
+++ game-jam/scripts/game_manager.gd
@@ -6,3 +6,17 @@ func add_point():
 	score += 1
 	print(score)
 	
+@onready var pause_menu = $CanvasLayer/PauseMenu
+var paused = false
+ 
+func _process(delta) :
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	paused = !paused 
